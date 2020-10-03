06204774243c7acbbac7411adacb082f4c10921b
phatdroid94 <phatdroid94@gmail.com>
2020-10-03 17:54:22 +0700

    object

---
+++ buoi1.js
@@ -0,0 +1,36 @@
+//ctrl + ` : mo terminal
+
+// 1 : Khai bao bien
+// mutable  : var
+// immutable : const
+// var a = 10 
+// const b = 10
+
+// a = "10"
+// b = "11"
+// console.log(a)
+// console.log(b)
+
+
+// 2 : Kieu du lieu
+// null
+// var a = null
+// console.log(a)
+// undefined
+// TH1 : khai bao 1 bien ma khong cap gia tri (chi ap dung cho var ,let)
+// var a 
+// console.log(a)
+// TH2 : Truy vấn tới thuộc tính không tồn tại của object
+// var teo = {
+//     name : "Nguyen Van Teo",
+//     age : 20
+// }
+// console.log(teo.name.a)
+
+//3 : Object (tim hieu ve camelcase)
+// JSON
+var teo = {
+    name : "Nguyen Van Teo",
+    age : 20
+}
+console.log(teo.name.a)
