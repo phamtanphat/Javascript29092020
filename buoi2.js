7612ccbbb964dcee70b798bbdba0915cd3c35522
phatdroid94 <phatdroid94@gmail.com>
2020-10-03 18:26:10 +0700

    operator

--- buoi2.js
+++ buoi2.js
@@ -1,8 +1,18 @@
 //4 : Array
-const arrNames = ["Teo","Ti","Tun","Tuan"]
+// const arrNames = ["Teo","Ti","Tun","Tuan"]
 // console.log(arrNames[0]) // truy cập thông thường
 
-const teo = {
-    name : "Nguyen Van Teo"
-}
-console.log(teo["name"])
+// const teo = {
+//     name : "Nguyen Van Teo"
+// }
+// console.log(teo["name"])
+
+// 5 : Arithmetic Operators :
+// Toán tử , toán hạng
+var a = 5
+var b = 5
+var ketqua = a++ - --b + a++ - b++ + --a - b-- + a--
+// 5 - --b + a++ - b++ + --a - b-- + a--
+// 5 - --b + a++ - b++ + --a - b-- + a--
+// 5 - --b + a++ - b++ + --a - b-- + a--
+// ket qua ?
