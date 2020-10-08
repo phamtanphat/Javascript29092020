11eb9ddf00e522ab4dd161e59637495ac589b394
phatdroid94 <phatdroid94@gmail.com>
2020-10-08 18:06:03 +0700

    bai tap ham map

--- buoi4.js
+++ buoi4.js
@@ -9,3 +9,36 @@
 //     return value * 2
 // })
 // console.log(newArrNums)
+
+// Ví dụ
+// Make an array of strings of the names
+// function namesOnly(arr) {
+//     const newArr =  arr.map(function(person , index){
+//         return person.name
+//     })
+//     console.log(newArr)
+// }
+
+// namesOnly([
+//   {
+//     name: "Angelina Jolie",
+//     age: 80
+//   },
+//   {
+//     name: "Eric Jones",
+//     age: 2
+//   },
+//   {
+//     name: "Paris Hilton",
+//     age: 5
+//   },
+//   {
+//     name: "Kayne West",
+//     age: 16
+//   },
+//   {
+//     name: "Bob Ziroll",
+//     age: 100
+//   }
+// ])
+// ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
