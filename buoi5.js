8e0bc25949d6908730e16f4f213f73b19d8934f4
phatdroid94 <phatdroid94@gmail.com>
2020-10-10 18:17:09 +0700

    function scope

--- buoi5.js
+++ buoi5.js
@@ -1,15 +1,25 @@
 // 19 : Prototype
-function Person(name , age){
-    this.name = name;
-    this.age = age;
-    this.setName = function(name){
-        this.name = name
-    }
-}
+// function Person(name , age){
+//     this.name = name;
+//     this.age = age;
+//     this.setName = function(name){
+//         this.name = name
+//     }
+// }
 
-Person.prototype.getInFo = function(){
-    return this
-}
-const teo = new Person("Nguyen Van Teo",20);
-const ti = new Person("Nguyen Van Ti",20);
-console.log(Person.prototype)
+// Person.prototype.getInFo = function (){
+//     return this
+// }
+// const teo = new Person("Nguyen Van Teo",20);
+// const ti = new Person("Nguyen Van Ti",20);
+// console.log(Person.prototype)
+
+// 20 : function scope
+// var a = 1
+// function random(){
+//   var a = Math.random()
+//   console.log(a)
+// }
+// random()
+// console.log(a)
+
